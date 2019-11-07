78ef9b2a66bceb37a80a7cde0435686aab5e102d
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-07 01:54:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def line(nelsonz_deli)
+  if nelsonz_deli.size > 0
+    line = "The line is currently:"
+    nelsonz_deli.each_with_index { |customer, i| line << " #{i+1}. #{customer}" }
+    puts line
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(nelsonz_deli, customer)
+  if !nelsonz_deli.include?(customer)
+    nelsonz_deli << customer
+    puts "Welcome, #{customer}. You are number #{nelsonz_deli.index(customer) + 1} in line."
+  end
+end
+
+def now_serving(nelsonz_deli)
+  nelsonz_deli
+end
